082a7b3eff43fa0240e89972c071ad8d2cef76ea
Douglas Potter <douglaspotter@gmail.com>
2022-06-06 17:56:42 +0200

    feat: prototype of node representation

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"math"
+
+	"github.com/snow-abstraction/optimal_set_partition/tree"
+)
+
+func main() {
+
+	unprocessed := tree.CreateInitialNodes()
+	k3, k4 := unprocessed[0].Branch(math.MaxFloat64, 3, 4)
+	unprocessed = append(unprocessed, k3, k4)
+
+	// for len(unprocessed) > 0 {
+
+	// 	node := unprocessed[0]
+	// 	unprocessed[0] = nil
+	// 	unprocessed = unprocessed[1:]
+
+	// 	// find lb
+	// 	// check if integral solution found
+	// 	// find branching rows/constraints
+	// 	// prune or branch
+
+	// 		printTree([]*Node{node})
+
+	// }
+
+	tree.PrintTree(unprocessed)
+}
---
+++ tree/nodes.go
@@ -0,0 +1,50 @@
+package tree
+
+import (
+	"math"
+)
+
+type NodeKind byte
+
+const (
+	root NodeKind = 0
+	// In the "both" branch subproblem the two branching
+	// constraints should be covered by the same variable
+	bothBranch = 1
+	// In the "diff" branch subproblem the two branching
+	// constrains should be covered by different variables
+	diffBranch = 2
+)
+
+// constraint branch-and-bound node
+// The subproblem the node represents can be calculated by applying
+// the branch type of it and its ancestors.
+type node struct {
+	kind       NodeKind
+	parent     *node // nil if root node
+	lowerBound float64
+	// The following have no meaning for the root node
+	branchConstraintOne uint32
+	branchConstraintTwo uint32
+}
+
+func createRoot() *node {
+	return &node{root, nil, math.MaxFloat64, math.MaxUint32, math.MaxUint32}
+}
+
+func CreateInitialNodes() []*node {
+	return []*node{createRoot()}
+}
+
+// Branches the parent on the two constrains to create two new Nodes
+func (parent *node) Branch(lowerBound float64, branchConstraintOne uint32,
+	branchConstraintTwo uint32) (*node, *node) {
+
+	if parent == nil {
+		panic("Cannot branch nil node.")
+	}
+
+	return &node{bothBranch, parent, lowerBound, branchConstraintOne, branchConstraintTwo},
+		&node{diffBranch, parent, lowerBound, branchConstraintOne, branchConstraintTwo}
+
+}
