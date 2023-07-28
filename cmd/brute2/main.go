5a8f29ce68397bde117bd386eb79fd99a7a5896c
Douglas Potter <douglaspotter@gmail.com>
2023-07-28 11:31:34 +0200

    feat: add a better brute force solver
    This adds the faster `brute2` solver.
    
    The `brute2` solver will soon replace the old `brute` solver and
    this is why `brute2` does not have a better name.
    
    The reason for having both `brute2` and `brute` is for
    showing how `brute2` was tested, showing that it returns the same
    solutions as `brute` (see `brute2_test.go`).
    
    The basis of `brute2` is testing combinations. This has much
    better complexity than the rather silly approach based on
    permutations used by `brute`.

---
+++ cmd/brute2/main.go
@@ -0,0 +1,83 @@
+/*
+ Copyright (C) 2023 Douglas Wayne Potter
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+// A brute force solver for the "Weighted Exact Cover Problem".
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/snow-abstraction/cover"
+	"github.com/snow-abstraction/cover/internal/solvers"
+)
+
+func usage() {
+	w := flag.CommandLine.Output()
+	fmt.Fprintf(
+		w,
+		`Usage: %s -instance instance.json
+
+%s reads in a problem instance JSON file, solves it and outputs a solution
+to standard out.
+
+Arguments:
+`,
+		os.Args[0],
+		os.Args[0])
+	flag.PrintDefaults()
+}
+
+func main() {
+	flag.Usage = usage
+	filename := flag.String("instance", "", "instance JSON filename")
+	flag.Parse()
+
+	if *filename == "" {
+		log.Fatalln("Please supply the instance file name")
+	}
+
+	b, err := os.ReadFile(*filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var ins cover.Instance
+	err = json.Unmarshal(b, &ins)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = json.Unmarshal(b, &ins)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	solverInstance, err := solvers.MakeInstance(ins.N, ins.Subsets, ins.Costs)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	sol, err := solvers.SolveByBruteForce2(solverInstance)
+	if err != nil {
+		fmt.Printf("failed to optimal solution due to error: %s", err)
+	}
+	fmt.Printf("Solution: %+v\n", sol)
+}
---
+++ internal/solvers/brute2.go
@@ -0,0 +1,116 @@
+/*
+ Copyright (C) 2023 Douglas Wayne Potter
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+// A brute force solver for the "Weighted Exact Cover Problem".
+package solvers
+
+import (
+	"golang.org/x/exp/slices"
+	"gonum.org/v1/gonum/stat/combin"
+)
+
+// makeSolutionFromSubsets attempts to make an exact cover by adding the candidates (subsets)
+// one by one (in order listed in subsetIndicies) until one of the three conditions are met:
+// 1. feasible (solution found),
+// 2. infeasible (overcovered or undercovered with no subsets left) or
+// 3. cost is greater or equal to that in the supplied in `best` if `best.ExactlyCovered`.
+//
+// It returns a subsetsEval with representing the cover found with ExactlyCovered == true found
+// for 1. and ExactlyCovered == false for 2. and 3.
+func makeSolutionFromSubsets2(ins instance, subsetIndices []int, best subsetsEval) subsetsEval {
+	coverCounts := make([]int, ins.n)
+
+	var s subsetsEval
+
+	if ins.n == 0 {
+		s.ExactlyCovered = true
+		return s
+	}
+
+	for _, subsetIdx := range subsetIndices {
+		for _, elementIdx := range ins.subsets[subsetIdx] {
+			coverCounts[elementIdx] += 1
+		}
+
+		s.Cost += ins.costs[subsetIdx]
+		// TODO: the use case is find a solution (aka a cover) that is cheaper than
+		// a known solution so we could abort the search if s.cost greater than the cost
+		// of a known cover.
+		s.SubsetsIndices = append(s.SubsetsIndices, subsetIdx)
+
+		allConstraintsCoveredExactly := true
+		for _, coverCount := range coverCounts {
+			isOverCovered := coverCount > 1
+			if isOverCovered {
+				return s
+			} else if coverCount == 0 {
+				allConstraintsCoveredExactly = false
+			}
+		}
+
+		if allConstraintsCoveredExactly {
+			s.ExactlyCovered = true
+			return s
+		}
+
+		if best.ExactlyCovered && best.Cost <= s.Cost {
+			return s
+		}
+	}
+
+	return s
+}
+
+// SolveByBruteForce attempts finds a minimum cost exact cover for
+// an instance.
+//
+// If a minimum cost exact cover exists, the returned subsetsEval will contain
+// indices to this cover and its exactlyCovered flag will be true. Otherwise,
+// the zero value of subsetEval will be returned.
+func SolveByBruteForce2(ins instance) (subsetsEval, error) {
+
+	nSubsetsToTry := ins.n
+	// At most len(ins.subsets) are needed because each subset has to cover
+	// at least one unique elemnt not covered by the other subsets in an
+	// exact cover.
+	if len(ins.subsets) < ins.n {
+		nSubsetsToTry = len(ins.subsets)
+	}
+
+	bestSubsetsEval := makeSolutionFromSubsets2(ins, nil, subsetsEval{})
+
+	for i := 1; i <= nSubsetsToTry; i++ {
+		combinations := combin.NewCombinationGenerator(len(ins.subsets), nSubsetsToTry)
+		for combinations.Next() {
+			perm := combinations.Combination(nil)
+			subsetEval := makeSolutionFromSubsets2(ins, perm, bestSubsetsEval)
+			if !subsetEval.ExactlyCovered {
+				continue
+			} else if !bestSubsetsEval.ExactlyCovered || subsetEval.Cost < bestSubsetsEval.Cost {
+				bestSubsetsEval = subsetEval
+			}
+
+		}
+	}
+
+	if !bestSubsetsEval.ExactlyCovered {
+		return subsetsEval{}, nil
+	}
+
+	slices.Sort(bestSubsetsEval.SubsetsIndices)
+	return bestSubsetsEval, nil
+}
