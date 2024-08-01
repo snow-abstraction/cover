61c8f7a79da63c42a4fe8110f6aaca7a60d29a1c
Douglas Potter <douglaspotter@gmail.com>
2024-08-01 21:43:22 +0200

    chore: update copyright year to 2024

--- internal/solvers/algo_test.go
+++ internal/solvers/algo_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2022 Douglas Wayne Potter
+Copyright (C) 2024 Douglas Wayne Potter
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
