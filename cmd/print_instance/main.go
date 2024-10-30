1118bb0edc2af1c444c4458eb8e3f450de2d5978
Douglas Wayne Potter <douglaspotter@gmail.com>
2024-10-30 17:28:47 +0100

    feat: add cmd to print JSON Instance in Go syntax

---
+++ cmd/print_instance/main.go
@@ -0,0 +1,117 @@
+/*
+Copyright (C) 2024 Douglas Wayne Potter
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/snow-abstraction/cover"
+)
+
+func usage() {
+	w := flag.CommandLine.Output()
+	fmt.Fprintf(
+		w,
+		`Usage: %s -instance instance.json
+
+%s reads in a problem instance JSON or MPS file and outputs it to standard out using
+Go debug formatting.
+
+Arguments:
+`,
+		os.Args[0],
+		os.Args[0])
+	flag.PrintDefaults()
+}
+
+func main() {
+
+	flag.Usage = usage
+	filename := flag.String("instance", "",
+		"instance filename. The file should end in .json (or .JSON) or .mps (or .MPS). MPS support is experimental.")
+	logLevel := flag.String("logLevel", "Info", "log level (Debug, Info, Warn, Error)")
+	flag.Parse()
+
+	level := parseLogLevel(*logLevel)
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     level,
+	})))
+
+	if *filename == "" {
+		fmt.Fprintln(os.Stderr, "Please supply the instance file name")
+		os.Exit(1)
+	}
+
+	ins, err := readInstance(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to read instance due to error: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Instance: %#v\n", ins)
+}
+
+func readInstance(filename *string) (*cover.Instance, error) {
+	ext := filepath.Ext(*filename)
+	lowerExt := strings.ToLower(ext)
+	switch lowerExt {
+	case ".json":
+		return readJsonInstance(filename)
+	case ".mps":
+		return cover.ReadMPSInstance(filename)
+	}
+
+	return nil, fmt.Errorf(
+		"the file extension should be .JSON, .json, .MPS or .mps and not %s", ext)
+
+}
+
+func readJsonInstance(filename *string) (*cover.Instance, error) {
+	b, err := os.ReadFile(*filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var ins cover.Instance
+	if err := json.Unmarshal(b, &ins); err != nil {
+		return nil, err
+	}
+	return &ins, nil
+}
+
+func parseLogLevel(level string) slog.Level {
+	switch level {
+	case "Debug":
+		return slog.LevelDebug
+	case "Info":
+		return slog.LevelInfo
+	case "Warn":
+		return slog.LevelWarn
+	case "Error":
+		return slog.LevelError
+	}
+	slog.Error("unknown log level. defaulting to Info")
+
+	return slog.LevelInfo
+}
